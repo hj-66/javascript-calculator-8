8ea8e007f54a189cc9f7c3c093dc84e9e02a2e1d
hj-66 <a01088200534@gmail.com>
2025-10-18 14:45:46 +0900

    feat: 문자열 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "./constants.js";
+
 class App {
-  async run() {}
+  async run() {
+    await this.getInput();
+  }
+
+  async getInput() {
+    this.input = await Console.readLineAsync(INPUT_MESSAGE.INPUT_STRING);
+  }
 }
 
 export default App;
--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const INPUT_MESSAGE = {
-  INPUT_STRING: "덧셈할 문자열을 입력해 주세요.\n",
+  INPUT_STRING: '덧셈할 문자열을 입력해 주세요.\n',
 };
 
 export const OUTPUT_MESSAGE = {
-  OUTPUT_RESULT: "결과 : ",
+  OUTPUT_RESULT: '결과 : ',
 };
