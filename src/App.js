4f132dabfbc6e49690516a9ec02d55114f0bedb0
hj-66 <a01088200534@gmail.com>
2025-10-18 18:24:08 +0900

    feat: 기본 구분자와 커스텀 구분자를 이용한 문자열 분리

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ class App {
   async getInput() {
     this.input = await Console.readLineAsync(INPUT_MESSAGE.INPUT_STRING);
   }
+
+  splitInput() {
+    if (this.input.startsWith('//')) {
+      const customIndex = this.input.indexOf('\\n'); 
+      const customSeparator = this.input.slice(2,customIndex);
+      const separatedIndex = this.input.slice(customIndex + 2).split(customSeparator);
+      return separatedIndex
+    }
+    return this.input.split(/,|:/);
+  }
 }
 
 export default App;
