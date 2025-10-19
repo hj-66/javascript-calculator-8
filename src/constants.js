39ccac2a0850fe8348d5e245fc5416968efb123a
hj-66 <a01088200534@gmail.com>
2025-10-19 20:21:52 +0900

    feat: 에러 메시지 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,9 @@ export const INPUT_MESSAGE = {
 export const OUTPUT_MESSAGE = {
   OUTPUT_RESULT: '결과 : ',
 };
+
+export const ERROR_MESSAGE = {
+  CUSTOM_ERROR_MESSAGE: '[ERROR] 커스텀 구분자 지정에 실패했습니다.',
+  CHARACTER_ERROR_MESSAGE: '[ERROR] 구분자가 아닌 다른 문자가 있습니다.',
+  NUMBER_ERROR_MESSAGE: '[ERROR] 숫자를 입력해 주세요.'
+}
