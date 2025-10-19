92f12eb8900d678d2d3ee5a0d3fa622859de029c
hj-66 <a01088200534@gmail.com>
2025-10-20 00:03:12 +0900

    feat: Error 구분 함수 추가

--- src/constants.js
+++ src/constants.js
@@ -8,6 +8,7 @@ export const OUTPUT_MESSAGE = {
 
 export const ERROR_MESSAGE = {
   CUSTOM_ERROR_MESSAGE: '[ERROR] 커스텀 구분자 지정에 실패했습니다.',
+  CUSTOMSETTING_ERROR_MESSAGE: '[ERROR] 커스텀 구분자 지정 형식이 잘못되었습니다.',
   CHARACTER_ERROR_MESSAGE: '[ERROR] 구분자가 아닌 다른 문자가 있습니다.',
   NUMBER_ERROR_MESSAGE: '[ERROR] 숫자를 입력해 주세요.'
 }
---
+++ src/validation.js
@@ -0,0 +1,42 @@
+import { ERROR_MESSAGE } from "./constants";
+
+export function validateAllowedCharacters(input, customDelimiter = null) {
+
+  const allowed = [',', ':', ... (customDelimiter ? [customDelimiter] : [])];
+
+  for (const ch of input) {
+    if (!/[0-9]/.test(ch) && !allowed.includes(ch)) {
+      throw new Error(ERROR_MESSAGE.CUSTOM_ERROR_MESSAGE);
+    }
+  }
+}
+
+export function validateIsNumber(str) {
+  if (!/^\d+$/.test(str)) {
+    throw new Error(ERROR_MESSAGE.NUMBER_ERROR_MESSAGE);
+  }
+}
+
+export function validateCustomDelimiterSyntax(input) {
+  if (input.startsWith('//')) {
+    if (!input.includes('\n')) {
+      throw new Error(ERROR_MESSAGE.CUSTOMSETTING_ERROR_MESSAGE);
+    }
+
+    const delimiterPart = input.split('\n')[0];
+
+    const match = /^\/\/(.)$/.exec(delimiterPart);
+    if (!match) {
+      throw new Error(ERROR_MESSAGE.CUSTOMSETTING_ERROR_MESSAGE);
+    }
+
+    const delimiter = match[1];
+
+    if (!isNaN(delimiter)) {
+      throw new Error(ERROR_MESSAGE.CUSTOM_ERROR_MESSAGE);
+    }
+
+    return delimiter;
+  }
+  return null;
+}
